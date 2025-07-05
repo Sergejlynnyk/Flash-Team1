7b81ec6759c1e4e63d0948ec94cea38a87fdfded
Nadiia Finko <finko.nv@gmail.com>
2025-07-05 12:30:23 +0200

    feat: add Not Found page and fix hero image path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Categories from './components/Categories/Categories';
@@ -12,7 +11,7 @@ import ProductsPage from './pages/ProductsPage';
 import ToolsAndEquipment from './components/ToolsAndEquipment/ToolsAndEquipment';
 import AllProducts from './components/AllProducts/AllProducts';
 
-// import Page404 from './components/Page404'; 
+import NotFound from './pages/NotFound/NotFound'; 
 
 import './index.css';
 
@@ -36,7 +35,6 @@ function CategoriesPage() {
   );
 }
 
-
 function App() {
   return (
     <Router>
@@ -49,7 +47,7 @@ function App() {
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/tools-and-equipment" element={<ToolsAndEquipment />} />
           <Route path="/all-products" element={<AllProducts />} />
-          {/* <Route path="*" element={<Page404 />} /> <- Korrekt! */}
+          <Route path="*" element={<NotFound />} /> {/* ✅ Добавлен маршрут */}
         </Routes>
 
         <Footer />
