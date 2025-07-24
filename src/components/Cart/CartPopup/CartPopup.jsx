9f70a340df1e2c8ea77a6cc6ebf3eb4a2f310c09
Nadiia Finko <finko.nv@gmail.com>
2025-07-24 06:36:10 +0200

    feat: show success popup

--- src/components/Cart/CartPopup/CartPopup.jsx
+++ src/components/Cart/CartPopup/CartPopup.jsx
@@ -3,7 +3,7 @@ import "./CartPopup.scss";
 
 const CartPopup = ({ onClose }) => {
   useEffect(() => {
-    const timer = setTimeout(onClose, 10000);
+    const timer = setTimeout(onClose, 10000); 
     return () => clearTimeout(timer);
   }, [onClose]);
 
@@ -11,14 +11,10 @@ const CartPopup = ({ onClose }) => {
     <div className="cart-popup">
       <div className="popup-content">
         <h2>Congratulations!</h2>
-        <p>
-          Your order has been successfully placed on the website. <br />A
-          manager will contact you shortly to confirm your order.
-        </p>
+        <p>Your order has been successfully placed on the website.</p>
+        <p>A manager will contact you shortly to confirm your order.</p>
       </div>
-      <button className="popup-close" onClick={onClose}>
-        ×
-      </button>
+      <button className="popup-close" onClick={onClose}>×</button>
     </div>
   );
 };
